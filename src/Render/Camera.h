a3e341e31d44e1d43804c82f0dd401582faabf6a
Coela Can't! <77935580+coelacant1@users.noreply.github.com>
2021-11-15 23:34:15 -0500

    Revert "Initial Commit"

--- src/Render/Camera.h
+++ src/Render/Camera.h
@@ -8,124 +8,123 @@
 #include "Triangle2D.h"
 
 class Camera {
-  private:
-   Transform* transform;
-   CameraLayout* cameraLayout;
-   PixelGroup* pixelGroup;
-   Quaternion rayDirection;
-   Quaternion lookDirection;
-   Quaternion lookOffset;
+private:
+    Transform* transform;
+    CameraLayout* cameraLayout;
+    PixelGroup* pixelGroup;
+    Quaternion rayDirection;
+    Quaternion lookDirection;
+    Quaternion lookOffset;
 
-   RGBColor CheckRasterPixel(Triangle2D** triangles, int numTriangles, Vector2D pixelRay) {
-      float zBuffer = 3.402823466e+38f;
-      int triangle = 0;
-      bool didIntersect = false;
-      float u = 0.0f, v = 0.0f, w = 0.0f;
-      Vector3D uvw;
-      RGBColor color;
-
-      for (int t = 0; t < numTriangles; t++) {
-         if (triangles[t]->DidIntersect(pixelRay.X, pixelRay.Y, u, v, w)) {
-            if (triangles[t]->averageDepth < zBuffer) {
-               uvw.X = u;
-               uvw.Y = v;
-               uvw.Z = w;
-               zBuffer = triangles[t]->averageDepth;
-               triangle = t;
-               didIntersect = true;
+    RGBColor CheckRasterPixel(Triangle2D** triangles, int numTriangles, Vector2D pixelRay){
+        float zBuffer = 3.402823466e+38f;
+        int triangle = 0;
+        bool didIntersect = false;
+        float u = 0.0f, v = 0.0f, w = 0.0f;
+        Vector3D uvw;
+        RGBColor color;
+        
+        for (int t = 0; t < numTriangles; t++) {
+            if (triangles[t]->DidIntersect(pixelRay.X, pixelRay.Y, u, v, w)){
+                if(triangles[t]->averageDepth < zBuffer){
+                    uvw.X = u;
+                    uvw.Y = v;
+                    uvw.Z = w;
+                    zBuffer = triangles[t]->averageDepth;
+                    triangle = t;
+                    didIntersect = true;
+                }
             }
-         }
-      }
+        }
 
-      if (didIntersect) {
-         Vector3D intersect = (*triangles[triangle]->t3p1 * uvw.X) + (*triangles[triangle]->t3p2 * uvw.Y) + (*triangles[triangle]->t3p3 * uvw.Z);
+        if(didIntersect){
+            Vector3D intersect = (*triangles[triangle]->t3p1 * uvw.X) + (*triangles[triangle]->t3p2 * uvw.Y) + (*triangles[triangle]->t3p3 * uvw.Z);
 
-         intersect = rayDirection.UnrotateVector(intersect);
-         Vector2D uv;
+            intersect = rayDirection.UnrotateVector(intersect);
+            Vector2D uv;
 
-         if (triangles[triangle]->hasUV) {
-            uv = *triangles[triangle]->p1UV * uvw.X + *triangles[triangle]->p2UV * uvw.Y + *triangles[triangle]->p3UV * uvw.Z;
-         }
-
-         color = triangles[triangle]->GetMaterial()->GetRGB(intersect, *triangles[triangle]->normal, Vector3D(uv.X, uv.Y, 0));
-      }
-
-      return color;
-   }
-
-  public:
-   Camera(Transform* transform, CameraLayout* cameraLayout, PixelGroup* pixelGroup) {
-      this->transform = transform;
-      this->pixelGroup = pixelGroup;
-      this->cameraLayout = cameraLayout;
-
-      transform->SetBaseRotation(cameraLayout->GetRotation());
-   }
-
-   Transform* GetTransform() { return transform; }
-
-   void SetLookOffset(Quaternion lookOffset) { this->lookOffset = lookOffset; }
-
-   void Rasterize(Scene* scene) {
-      int numTriangles = 0;
-
-      // for each object in the scene, get the triangles
-      for (int i = 0; i < scene->GetObjectCount(); i++) {
-         if (scene->GetObjects()[i]->IsEnabled()) {
-            numTriangles += scene->GetObjects()[i]->GetTriangleGroup()->GetTriangleCount();
-         }
-      }
-
-      Triangle2D** triangles = new Triangle2D*[numTriangles];
-      int triangleCounter = 0;
-
-      lookDirection = transform->GetRotation().Conjugate() * lookOffset;
-      rayDirection = transform->GetRotation().Multiply(lookDirection);
-
-      // for each object in the scene, get the triangles
-      for (int i = 0; i < scene->GetObjectCount(); i++) {
-         if (scene->GetObjects()[i]->IsEnabled()) {
-            // for each triangle in object, project onto 2d surface, but pass material
-            for (int j = 0; j < scene->GetObjects()[i]->GetTriangleGroup()->GetTriangleCount(); j++) {
-               triangles[triangleCounter] = new Triangle2D(lookDirection, transform, &scene->GetObjects()[i]->GetTriangleGroup()->GetTriangles()[j], scene->GetObjects()[i]->GetMaterial());
-
-               bool triangleInView = false;
-
-               if (triangles[triangleCounter]->averageDepth > 0) {  // cull behind camera
-                  Vector2D p1 = lookDirection.UnrotateVector(triangles[triangleCounter]->GetP1()) * transform->GetScale();
-                  Vector2D p2 = lookDirection.UnrotateVector(triangles[triangleCounter]->GetP2()) * transform->GetScale();
-                  Vector2D p3 = lookDirection.UnrotateVector(triangles[triangleCounter]->GetP3()) * transform->GetScale();
-
-                  BoundingBox2D triangleBox;
-
-                  triangleBox.UpdateBounds(p1);
-                  triangleBox.UpdateBounds(p2);
-                  triangleBox.UpdateBounds(p3);
-
-                  triangleInView = pixelGroup->Overlaps(&triangleBox);  // cull outside camera view
-               }
-
-               if (triangleInView)
-                  triangleCounter++;
-               else
-                  delete triangles[triangleCounter];  // out of view space remove from array
+            if (triangles[triangle]->hasUV){
+                uv = *triangles[triangle]->p1UV * uvw.X + *triangles[triangle]->p2UV * uvw.Y + *triangles[triangle]->p3UV * uvw.Z;
             }
-         }
-      }
+            
+            color = triangles[triangle]->GetMaterial()->GetRGB(intersect, *triangles[triangle]->normal, Vector3D(uv.X, uv.Y, 0));
+        }
+        
+        return color;
+    }
 
-      for (unsigned int i = 0; i < pixelGroup->GetPixelCount(); i++) {
-         Vector2D pixelRay = Vector2D(lookDirection.RotateVector(pixelGroup->GetPixel(i)->GetPosition() * transform->GetScale()));  // scale pixel location prior to rotating and moving
-         pixelGroup->GetPixel(i)->Color = CheckRasterPixel(triangles, triangleCounter, pixelRay);
-      }
+public:
+    Camera(Transform* transform, CameraLayout* cameraLayout, PixelGroup* pixelGroup){
+        this->transform = transform;
+        this->pixelGroup = pixelGroup;
+        this->cameraLayout = cameraLayout;
 
-      for (int i = 0; i < triangleCounter; i++) {
-         delete triangles[i];
-      }
+        transform->SetBaseRotation(cameraLayout->GetRotation());
+    }
 
-      delete[] triangles;
-   }
+    Transform* GetTransform(){
+        return transform;
+    }
 
-   void SwapPixels(PixelGroup* inPixels) {
-      this->pixelGroup = inPixels;
-   }
+    void SetLookOffset(Quaternion lookOffset){
+        this->lookOffset = lookOffset;
+    }
+
+    void Rasterize(Scene* scene) {
+        int numTriangles = 0;
+    
+        //for each object in the scene, get the triangles
+        for(int i = 0; i < scene->GetObjectCount(); i++){
+            if(scene->GetObjects()[i]->IsEnabled()){
+                numTriangles += scene->GetObjects()[i]->GetTriangleGroup()->GetTriangleCount();
+            }
+        }
+        
+        Triangle2D** triangles = new Triangle2D*[numTriangles];
+        int triangleCounter = 0;
+        
+        lookDirection = transform->GetRotation().Conjugate() * lookOffset;
+        rayDirection  = transform->GetRotation().Multiply(lookDirection);
+
+        //for each object in the scene, get the triangles
+        for(int i = 0; i < scene->GetObjectCount(); i++){
+            if(scene->GetObjects()[i]->IsEnabled()){
+                //for each triangle in object, project onto 2d surface, but pass material
+                for (int j = 0; j < scene->GetObjects()[i]->GetTriangleGroup()->GetTriangleCount(); j++) {
+                    triangles[triangleCounter] = new Triangle2D(lookDirection, transform, &scene->GetObjects()[i]->GetTriangleGroup()->GetTriangles()[j], scene->GetObjects()[i]->GetMaterial());
+                    
+                    bool triangleInView = false;
+
+                    if (triangles[triangleCounter]->averageDepth > 0){//cull behind camera
+                        Vector2D p1 = lookDirection.UnrotateVector(triangles[triangleCounter]->GetP1()) * transform->GetScale();
+                        Vector2D p2 = lookDirection.UnrotateVector(triangles[triangleCounter]->GetP2()) * transform->GetScale();
+                        Vector2D p3 = lookDirection.UnrotateVector(triangles[triangleCounter]->GetP3()) * transform->GetScale();
+
+                        BoundingBox2D triangleBox;
+
+                        triangleBox.UpdateBounds(p1);
+                        triangleBox.UpdateBounds(p2);
+                        triangleBox.UpdateBounds(p3);
+
+                        triangleInView = pixelGroup->Overlaps(&triangleBox);//cull outside camera view
+                    }
+                    
+                    if(triangleInView) triangleCounter++;
+                    else delete triangles[triangleCounter];//out of view space remove from array
+                }
+            }
+        }
+
+        for (unsigned int i = 0; i < pixelGroup->GetPixelCount(); i++) {
+            Vector2D pixelRay = Vector2D(lookDirection.RotateVector(pixelGroup->GetPixel(i)->GetPosition() * transform->GetScale()));//scale pixel location prior to rotating and moving
+            pixelGroup->GetPixel(i)->Color = CheckRasterPixel(triangles, triangleCounter, pixelRay);
+        }
+
+        for (int i = 0; i < triangleCounter; i++){
+            delete triangles[i];
+        }
+        
+        delete[] triangles;
+    }
+    
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-#include "Animation\CoelaBonkAnimation.h"
-#include "Controllers\MyloController.h"
-#include "soc/rtc_wdt.h"
-#define MAX_BRIGHT 31
-#define DITHER true
-
-Animation* bonk = new CoelaBonkAnimation();
-MyloController* controller = new MyloController();
-
-TaskHandle_t Task1;
-TaskHandle_t Task2;
-
-boolean frameReady = false;
-boolean frameAck = false;
-
-void renderThread(void* pvParameters);
-void displayThread(void* pvParameters);
-=======
 #include "Animation\ProtoDRMorphAnimation.h"
 //#include "Animation\KaiborgV1Animation.h"
 //include "Animation\ProtoV3Animation.h"
@@ -38,51 +19,24 @@ Controller* controller = new ProtoDRController(maxBrightness, ProtoDRController:
 //Controller* controller = new KaiborgV1D1Controller(maxBrightness);
 //ProtoDRMorphAnimation protoDRMorph;
 Animation* animation = new ProtoDRMorphAnimation();
->>>>>>> ESP32
 
 void setup() {
-   Serial.begin(115200);
-   Serial.println("\nStarting...");
-   controller->Initialize(MAX_BRIGHT, DITHER);
+    Serial.begin(115200);
+    Serial.println("\nStarting...");
 
-   xTaskCreatePinnedToCore(displayThread, "DisplayThread", 10000, NULL, 1, &Task2, 0);
-   delay(10);
-   xTaskCreatePinnedToCore(renderThread, "RenderThread", 10000, NULL, 1, &Task1, 1);
+    controller->Initialize();
 }
 
-void renderThread(void* pvParameters) {
-   while (true) {
-      for (float i = 0.0f; i < 1.0f; i += 1.0f / 720.0f) {
-         bonk->Update(i);
+void loop() {
+    for (float i = 0.0f; i < 1.0f; i += 1.0f / 720.0f) {
+        animation->Update(i);
 
-         controller->Render(bonk->GetScene());
-         frameReady = true;
+        controller->Render(animation->GetScene());
 
-         while (true) {
-            if (frameAck)
-               break;
-            else {
-               delay(1);
-            }
-         }
-         frameReady = false;
-         frameAck = false;
-      }
-   }
+        controller->Display();
+
+        Serial.print("Rendered in ");
+        Serial.print(controller->GetRenderTime(), 4);
+        Serial.println("s");
+    }
 }
-
-void displayThread(void* pvParameters) {
-   while (true) {
-      if (frameReady) {
-         controller->SwapBuffers();
-         frameAck = true;
-         frameReady = false;
-         controller->Display();
-      } else {
-         delay(1);
-         FastLED.show();
-      }
-   }
-}
-
-void loop() {}
