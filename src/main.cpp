c38672838ef5937c8ed5adf6ffa7e23a1ad7876b
Daniel Größinger <daniel.groessinger@gmail.com>
2024-05-16 19:45:16 +0200

    add simple main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "Examples\UserConfiguration.h"
+#include "..\lib\ProtoTracer\ExternalDevices\InputDevices\ButtonHandler.h"
 
 #ifdef TESTHARDWARE
 #include "Examples\Protogen\ProtogenHardwareTest.h"
@@ -6,7 +7,6 @@
 
 //#include "Examples\Commissions\UnicornZhenjaAnimation.h"
 #include "Examples\Protogen\ProtogenHUB75Project.h"
-//#include "Examples\Protogen\ProtogenWS35Project.h"
 //#include "Examples\VerifyEngine.h"
 
 ProtogenHUB75Project project;
@@ -14,21 +14,22 @@ ProtogenHUB75Project project;
 void setup() {
     Serial.begin(115200);
     Serial.println("\nStarting...");
+    pinMode(14, INPUT_PULLDOWN);
     
-    #ifndef TESTHARDWARE
     project.Initialize();
     delay(100);
-    #else
-    while(true){
-        HardwareTest::ScanDevices();
-        HardwareTest::TestNeoTrellis();
-        HardwareTest::TestBoopSensor();
-        HardwareTest::TestHUD();
-    }
-    #endif
 }
 
-void loop() {
+void loop(){
+
+    
+    // project.CustomColor(customMaterial);
+    // project.SelectColor(9);
+
+    // ButtonHandler::Initialize(14, 100, 100);
+     
+    project.SelectColor(5); // Set white color
+
     float ratio = (float)(millis() % 5000) / 5000.0f;
 
     project.Animate(ratio); 
